5738ffdc6f3d1da39e626b996538670b1872cd17
Hasindu Lakshan <hasindulakshan0040@gmail.com>
2025-02-06 12:56:09 +0530

    built-in classes

---
+++ 01. Built-in classes.py
@@ -0,0 +1,8 @@
+# there are the built-in classes in python, such as int, float, str, list, tuple, dict etc.
+
+name = "John"  # str
+age = 23  # int
+
+# The type() function returns the class of the object.
+print(type(name))  # <class 'str'>
+print(type(age))  # <class 'int'>
