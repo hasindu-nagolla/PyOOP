b3c25a90a8ee48d10dc21cdeab824cb98cddd5df
Hasindu Lakshan <hasindulakshan0040@gmail.com>
2025-02-28 22:25:49 +0530

    Static vs. Instance Methods

---
+++ 05. Static vs. Instance Methods/index.py
@@ -0,0 +1,6 @@
+# ==============================================================================
+# Static Methods 
+# ==============================================================================
+
+#  Belong to the class itself, but dont access instance(self) or class(cls) attributes
+# Defined using @staticmethod decorator
